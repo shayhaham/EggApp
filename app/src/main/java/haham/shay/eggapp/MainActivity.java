e0189325ecb48e768c650c03b8d7d8d73443bf30
שי חכם <shayhaham956@gmail.com>
2024-09-30 16:21:48 +0300

    final version1

--- app/src/main/java/haham/shay/eggapp/MainActivity.java
+++ app/src/main/java/haham/shay/eggapp/MainActivity.java
@@ -1,17 +1,21 @@
 package haham.shay.eggapp;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    private Button btnGetOut;
     private int counter;
 
     @Override
@@ -20,26 +24,57 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         counter = 10;
         TextView counterTextView = findViewById(R.id.textview_counter);
-        counterTextView.setText(" "+counter);
+        counterTextView.setText(" " + counter);
+        btnGetOut = (Button) findViewById(R.id.btnGETOUT);
+        EvantHandler();
 
     }
-    public void knock (View view) {
-        if(counter>0){
+
+    public void knock(View view) {
+        if (counter > 0) {
             counter--;
             TextView counterTextView = findViewById(R.id.textview_counter);
-            counterTextView.setText(" "+counter);
-            if(counter==0){
+            counterTextView.setText(" " + counter);
+            if (counter == 0) {
                 ImageView eggimageview = findViewById(R.id.imageView_egg);
                 eggimageview.setImageResource(R.drawable.surprise_egg);
             }
         }
 
     }
-    public void reset (View view){
-        counter= 10;
+
+    public void reset(View view) {
+        counter = 10;
         TextView counterTextView = findViewById(R.id.textview_counter);
-        counterTextView.setText(" "+counter);
+        counterTextView.setText(" " + counter);
         ImageView eggimageview = findViewById(R.id.imageView_egg);
         eggimageview.setImageResource(R.drawable.green_egg);
+    }
+
+    private void EvantHandler() {
+        btnGetOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder Builder = new AlertDialog.Builder(MainActivity.this);
+                Builder.setMessage("Are You Sure You Wanna Leave?")
+                        .setCancelable(false)
+                        .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                MainActivity.this.finish();
+                            }
+                        })
+                        .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.cancel();
+                            }
+                        });
+                AlertDialog Alert = Builder.create();
+                Alert.show();
+            }
+
+            ;
+        });
     }
 }
